185541b56b508f31ab02fd4000af86b60651f7c7
Artem Gluhotorenko <dabygi2010@gmail.com>
2022-01-18 00:53:10 +0300

    ticket edits: serializers and views

--- tickets/views.py
+++ tickets/views.py
@@ -1,6 +1,27 @@
+from tickets.models import Ticket
+from tickets.serializers import TicketCreateSerializer, TicketListSerializer, TicketDetailSerializer
+from rest_framework.views import APIView
 from rest_framework import generics
-from tickets.serializers import TicketCreateSerializer
+from rest_framework.response import Response
 
 
 class TicketCreateView(generics.CreateAPIView):
     serializer_class = TicketCreateSerializer
+
+
+class TicketListView(APIView):
+    """Вывод полного списка тикетов"""
+
+    def get(self, request):
+        tickets = Ticket.objects.all()
+        serializer = TicketListSerializer(tickets, many=True)
+        return Response(serializer.data)
+
+
+class TicketDetailView(APIView):
+    """Вывод конкретного тикета"""
+
+    def get(self, request, pk):
+        tickets = Ticket.objects.get(id=pk)
+        serializer = TicketDetailSerializer(tickets)
+        return Response(serializer.data)
