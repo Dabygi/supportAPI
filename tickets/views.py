4d5a25197e84fab0260ce76a64a9d7f0abb53f7b
Artem Gluhotorenko <dabygi2010@gmail.com>
2022-01-25 22:30:44 +0300

    correct views tickets on generic-class

--- tickets/views.py
+++ tickets/views.py
@@ -3,50 +3,42 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import generics
 from tickets.models import Ticket
 from tickets.serializers import TicketCreateSerializer, TicketListSerializer, TicketDetailSerializer, \
     CommentCreateSerializer
 
 
-class TicketCreateView(APIView):
+class TicketCreateView(generics.CreateAPIView):
     """Создать тикет"""
 
-    def post(self, request):
-        ticket = TicketCreateSerializer(data=request.data)
-        if ticket.is_valid():
-            ticket.save()
-            return Response(status=201)
+    serializer_class = TicketCreateSerializer
+    permission_classes = (AllowAny,)
 
 
-class TicketListView(APIView):
+class TicketListView(generics.ListAPIView):
     """Вывод полного списка тикетов"""
 
+    serializer_class = TicketListSerializer
     permission_classes = (AllowAny,)
     filter_backends = [DjangoFilterBackend]
     filterset_fields = ['category', 'subcategory', 'ticket_status']
 
-    def get(self, request):
+    def get_queryset(self):
         tickets = Ticket.objects.all()
-        serializer = TicketListSerializer(tickets, many=True)
-        return Response(serializer.data)
+        return tickets
 
 
-class TicketDetailView(APIView):
+class TicketDetailView(generics.RetrieveAPIView):
     """Вывод конкретного тикета"""
 
-    def get(self, request, pk):
-        tickets = Ticket.objects.get(id=pk)
-        serializer = TicketDetailSerializer(tickets)
-        return Response(serializer.data)
+    serializer_class = TicketDetailSerializer
+    queryset = Ticket.objects.filter()
 
 
-class CommentCreateView(APIView):
+class CommentCreateView(generics.CreateAPIView):
     """Добавление комментария"""
 
-    def post(self, request):
-        comment = CommentCreateSerializer(data=request.data)
-        if comment.is_valid():
-            comment.save()
-            return Response(status=201)
+    serializer_class = CommentCreateSerializer
 
 
